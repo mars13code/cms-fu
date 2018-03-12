b7107357435f300acf49f0b5c3c7a1efbe912117
mars13 <cloud9@mars13.fr>
2018-03-12 00:35:17 +0000

    M projet/theme/base/view/section-defaut.php

--- projet/theme/base/view/section-defaut.php
+++ projet/theme/base/view/section-defaut.php
@@ -10,10 +10,12 @@
         <h3>afficherOption</h3>
         <?php afficherOption("page.contenu") ?>
     </content>
+    <?php bloc("bloc.test2") ?>
     <content>
         <h3>filtrerInfo</h3>
         <h4><?php filtrerInfo("page.titre") ?></h4>
         <?php filtrerInfo("page.contenu") ?>
     </content>
+    <?php bloc("bloc.test2") ?>
 </section>
 <?php bloc("bloc.blablabla") ?>
