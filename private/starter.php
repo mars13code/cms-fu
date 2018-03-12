e389b38076ea29d8f5842dbee5558aa541834168
mars13 <cloud9@mars13.fr>
2018-03-12 15:41:22 +0000

    M .gitignore  M private/starter.php  M projet/theme/base/view-template/ajax.php  M projet/theme/base/view/crud-Upload.php

--- private/starter.php
+++ private/starter.php
@@ -1,33 +1,33 @@
 <?php
 
-global  $rootDir,   $cmsMode, 
-        $dbSQL,     $userSQL,   $passSQL;
+global $rootDir, $cmsMode,
+$dbSQL, $userSQL, $passSQL;
 
 $cmsMode ?? $cmsMode = "PROD";
 // sur un vrai site, on est directement a la racine
 $rootDir ?? $rootDir = "";
 
-if ($cmsMode == "DEV")
-{
+if ($cmsMode == "DEV") {
     // mode dev
     error_reporting(E_ALL);
     @ini_set('display_errors', '1');
-    @ini_set('log_errors','Off'); // enable or disable php error logging (use 'On' or 'Off')
+    @ini_set('log_errors', 'Off'); // enable or disable php error logging (use 'On' or 'Off')
 }
 
 // variables globales
 
-$dbSQL      ?? $dbSQL   = "cmsFun";
-$userSQL    ?? $userSQL = "root";
-$passSQL    ?? $passSQL = "";
+$dbSQL ?? $dbSQL     = "cmsFun";
+$userSQL ?? $userSQL = "root";
+$passSQL ?? $passSQL = "";
 
 //$dossierTheme = "../private";
 $dossierTheme = "../projet/theme";
+$dossierCMS   = dirname(__DIR__);
 
 // charger les déclarations de mes fonctions
-require_once(__DIR__ . "/model-functions.php");
-require_once(__DIR__ . "/view-functions.php");
-require_once(__DIR__ . "/controller-functions.php");
+require_once __DIR__ . "/model-functions.php";
+require_once __DIR__ . "/view-functions.php";
+require_once __DIR__ . "/controller-functions.php";
 
 // variables globales
 // dossiers qui vont grouper les fichiers de traitement de formulaire
--- projet/theme/base/view/crud-Upload.php
+++ projet/theme/base/view/crud-Upload.php
@@ -1,4 +1,111 @@
-
+<?php
+// https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+?>
 <section>
     <h3>UPLOAD</h3>
+    <style>
+#drop-area {
+  border: 2px dashed #ccc;
+  border-radius: 20px;
+  width: 480px;
+  font-family: sans-serif;
+  margin: 100px auto;
+  padding: 20px;
+}
+#drop-area.highlight {
+  border-color: purple;
+}
+p {
+  margin-top: 0;
+}
+.my-form {
+  margin-bottom: 10px;
+}
+#gallery {
+  margin-top: 10px;
+}
+#gallery img {
+  width: 150px;
+  margin-bottom: 10px;
+  margin-right: 10px;
+  vertical-align: middle;
+}
+.button {
+  display: inline-block;
+  padding: 10px;
+  background: #ccc;
+  cursor: pointer;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+.button:hover {
+  background: #ddd;
+}
+#fileElem {
+  display: none;
+}        
+    </style>
+    <div id="drop-area">
+        <form class="my-form">
+            <p>Upload multiple files with the file dialog or by dragging and dropping images onto the dashed region</p>
+            <input type="file" id="fileElem" multiple accept="image/*" onchange="handleFiles(this.files)">
+            <label class="button" for="fileElem">Select some files</label>
+        </form>
+    </div>
+    <script>
+let dropArea = document.getElementById('drop-area');
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false)
+})
+
+function preventDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+;['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false)
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false)
+})
+
+function highlight(e) {
+  dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove('highlight')
+}
+
+dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+  let dt = e.dataTransfer
+  let files = dt.files
+
+  handleFiles(files)
+}
+
+function handleFiles(files) {
+  ([...files]).forEach(uploadFile)
+}
+
+function uploadFile(file) {
+  let url = 'ajax'
+  let formData = new FormData()
+
+  formData.append('file', file)
+
+  fetch(url, {
+    mode: 'no-cors',  
+    method: 'POST',
+    body: formData
+  })
+  .then(() => { console.log("UPLOAD DONE") })
+  .catch(() => { console.log("UPLOAD ERROR") })
+}
+
+    </script>
 </section>
