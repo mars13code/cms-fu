ff92000fcb3f68e01c83af8a41820a5b2994c538
mars13 <cloud9@mars13.fr>
2018-03-14 21:43:32 +0000

    M private/functions-controller.php

--- private/functions-controller.php
+++ private/functions-controller.php
@@ -1,5 +1,17 @@
 <?php
 
+// http://www.weirdog.com/blog/php/supprimer-les-accents-des-caracteres-accentues.html
+function wd_remove_accents($str, $charset='utf-8')
+{
+    $str = htmlentities($str, ENT_NOQUOTES, $charset);
+    
+    $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+    $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
+    $str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
+    
+    return $str;
+}
+
 function filtrerUpload($inputName)
 {
     $result = "";
